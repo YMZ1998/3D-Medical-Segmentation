670b96363c3fc4c0a1d7404b2978f928fbd040e0
李进 <1320439118@qq.com>
2025-02-24 09:08:51 +0800

    init.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# 3D-Medical-Segmentation
+# [3D-Medical-Segmentation](https://github.com/YMZ1998/3D-Medical-Segmentation)
 Some implementations of 3d medical segmentation.
---
+++ make_dataset.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+
+def remove_and_create_dir(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path, exist_ok=True)
+
+
+src = r"D:\Data\CTV_Seg"
+dst = r"./datasets"
+
+remove_and_create_dir(dst)
+
+for mode in ["Train", "Test"]:
+    src_dir = os.path.join(src, mode)
+    dst_dir = os.path.join(dst, mode)
+    os.makedirs(dst_dir, exist_ok=True)
+    os.makedirs(os.path.join(dst_dir, "image"), exist_ok=True)
+    os.makedirs(os.path.join(dst_dir, "label"), exist_ok=True)
+
+    i = 0
+    for p in os.listdir(src_dir):
+        i = i + 1
+        image_dir = os.path.join(src, mode, p, "image.nii.gz")
+        label_dir = os.path.join(src, mode, p, "CTV.nii.gz")
+        print(i, image_dir, label_dir)
+
+        dst_image_dir = os.path.join(dst_dir, "image", str(i).rjust(3, "0") + ".nii.gz")
+        dst_label_dir = os.path.join(dst_dir, "label", str(i).rjust(3, "0") + ".nii.gz")
+        shutil.copyfile(image_dir, dst_image_dir)
+        shutil.copyfile(label_dir, dst_label_dir)
